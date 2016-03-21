3db7f7c49b9a22f446cecd042a340740c7fd27f6
bumperburke <stefan.burke@mydit.ie>
2016-03-21 20:18:03 +0000

    GeoBus Server V1.0

--- public/api/include/DbOps.php
+++ public/api/include/DbOps.php
@@ -45,7 +45,7 @@ class DbOps {
 			return true;
 		}else{
 			return false;
-        }
+        	}
 	}
 
 	public function checkLoginCreds($email){
@@ -78,8 +78,13 @@ class DbOps {
 		$this->conn->bindVal($query, 1, $token, $this->conn->STRING);
 		$this->conn->executeQuery($query);
 		$fetchedResults = $this->conn->fetchResults($query);
+		$tokenTime = $fetchedResults[0]["tokenExpire"];
+		$tokenTime = strtotime($tokenTime);
+
 		$currentTime = date('Y-m-d H:i:s');
-		if($currentTime > $fetchedResults['tokenExpire']){
+		$currentTime = strtotime($currentTime);
+
+		if($currentTime > $tokenTime){
 			return true;
 		}
 		else{
@@ -110,14 +115,34 @@ class DbOps {
 		}
 	}
 
-	public function updateLocation($lat, $lon, $time, $busID){
-		$location = 'POINT('.$lat." ".$lon.')';
-		$stmt = "INSERT INTO location (geoLocation, timestamp, busID) VALUES (PointFromText(?), ?, ?)";
+	public function updateLocation($lat, $lon, $time, $deviceID, $routeID){
+		$stmt = "SELECT deviceID FROM location WHERE deviceID = ?;";
 		$query = $this->conn->prepareQuery($stmt);
-		$this->conn->bindVal($query, 1, $location, $this->conn->STRING);
-		$this->conn->bindVal($query, 2, $time, $this->conn->STRING);
-		$this->conn->bindVal($query, 3, $busID, $this->conn->INT);
+		$this->conn->bindVal($query, 1, $deviceID, $this->conn->INT);
 		$this->conn->executeQuery($query);
+		$fetchedResults = $this->conn->fetchResults($query);
+		$location = 'POINT('.$lat." ".$lon.')';
+
+		if(empty($fetchedResults)){
+			$stmt = "INSERT INTO location (routeID, deviceID, geoLocation, timestamp) VALUES (?, ?, PointFromText(?), ?)";
+			$query = $this->conn->prepareQuery($stmt);
+			$this->conn->bindVal($query, 1, $routeID, $this->conn->INT);
+			$this->conn->bindVal($query, 2, $deviceID, $this->conn->INT);
+			$this->conn->bindVal($query, 3, $location, $this->conn->STRING);
+			$this->conn->bindVal($query, 4, $time, $this->conn->STRING);
+			$this->conn->executeQuery($query);
+		}else{
+			$devID = $fetchedResults[0]["deviceID"];
+			$stmt = "UPDATE location SET routeID = ?, deviceID = ?, geoLocation = PointFromText(?), timestamp = ? ";
+			$stmt .= "WHERE deviceID = ?;";
+			$query = $this->conn->prepareQuery($stmt);
+			$this->conn->bindVal($query, 1, $routeID, $this->conn->INT);
+			$this->conn->bindVal($query, 2, $deviceID, $this->conn->INT);
+			$this->conn->bindVal($query, 3, $location, $this->conn->STRING);
+			$this->conn->bindVal($query, 4, $time, $this->conn->STRING);
+			$this->conn->bindVal($query, 5, $devID, $this->conn->INT);
+			$this->conn->executeQuery($query);
+		}
 
 		return "success";
 	}
@@ -132,22 +157,32 @@ class DbOps {
 		return "success";
 	}
 
-	public function getLocation(){
-		$stmt = "SELECT ST_Y(geoLocation) as Ypos, ST_X(geoLocation) as Xpos FROM location WHERE busID = 1 AND locID <= 26";
-		//$stmt = "SELECT ST_X(location) as Xpos, ST_Y(location) as Ypos FROM routeMap;";
-		$query = $this->conn->prepareQuery($stmt);
-		$this->conn->executeQuery($query);
-		$locations = $this->conn->fetchResults($query);
-		return $locations;
+	public function getLocation($route){
+		if(intval($route) == 0){
+			$routeID = $this->fetchRouteID($route);
+
+			if($routeID == null){
+				return false;
+			}else{
+				$stmt = "SELECT ST_X(geoLocation) as Xpos, ST_Y(geoLocation) as Ypos, timestamp, deviceID FROM location WHERE routeID = ?;";
+				$query = $this->conn->prepareQuery($stmt);
+				$this->conn->bindVal($query, 1, $routeID, $this->conn->INT);
+				$this->conn->executeQuery($query);
+				$location = $this->conn->fetchResults($query);
+				return $location;
+			}
+		}else{
+			$stmt = "SELECT ST_X(geoLocation) as Xpos, ST_Y(geoLocation) as Ypos, timestamp FROM location WHERE deviceID = ?;";
+			$query = $this->conn->prepareQuery($stmt);
+			$this->conn->bindVal($query, 1, $route, $this->conn->INT);
+			$this->conn->executeQuery($query);
+			$location = $this->conn->fetchResults($query);
+			return $location;
+		}
 	}
 
 	public function getRoute($route){
-		$stmt = "SELECT routeID FROM route WHERE routeName LIKE ?;";
-		$query = $this->conn->prepareQuery($stmt);
-		$this->conn->bindVal($query, 1, $route, $this->conn->STRING);
-		$this->conn->executeQuery($query);
-		$fetchedResults = $this->conn->fetchResults($query);
-		$routeID = intval($fetchedResults[0]['routeID']);
+		$routeID = $this->fetchRouteID($route);
 
 		$stmt = "SELECT ST_X(location) as Xpos, ST_Y(location) as Ypos ";
 		$stmt .= "FROM routeMap WHERE routeID = ?;";
@@ -159,39 +194,39 @@ class DbOps {
 	}
 
 	public function getStops($route){
-		$stmt = "SELECT routeID FROM route WHERE routeName LIKE ?;";
-		$query = $this->conn->prepareQuery($stmt);
-		$this->conn->bindVal($query, 1, $route, $this->conn->STRING);
-		$this->conn->executeQuery($query);
-		$fetchedResults = $this->conn->fetchResults($query);
-		$routeID = intval($fetchedResults[0]['routeID']);
+		$routeID = $this->fetchRouteID($route);
+		$data = null;
+		if($routeID == 3){
+			$stmt = "SELECT name, ST_X(location) AS Xpos, ST_Y(location) AS Ypos FROM stop WHERE stopID <=8 UNION ";
+			$stmt .= "SELECT name, ST_X(location) AS Xpos, ST_Y(location) AS Ypos FROM stop WHERE routeID = ?;";
+			$query = $this->conn->prepareQuery($stmt);
+			$this->conn->bindVal($query, 1, $routeID, $this->conn->INT);
+			$this->conn->executeQuery($query);
+			$data = $this->conn->fetchResults($query);
+		}else{
+			$stmt = "SELECT name, ST_X(location) as Xpos, ST_Y(location) as Ypos FROM stop WHERE routeID = ?;";
+			$query = $this->conn->prepareQuery($stmt);
+			$this->conn->bindVal($query, 1, $routeID, $this->conn->INT);
+			$this->conn->executeQuery($query);
+			$data = $this->conn->fetchResults($query);
+		}
 
-		$stmt = "SELECT name, ST_X(location) as Xpos, ST_Y(location) as Ypos FROM stop WHERE routeID = ?;";
-		$query = $this->conn->prepareQuery($stmt);
-		$this->conn->bindVal($query, 1, $routeID, $this->conn->INT);
-		$this->conn->executeQuery($query);
-		$data = $this->conn->fetchResults($query);
 		return $data;
 	}
 
 	public function getTimetable($timetable){
-		$stmt = "SELECT routeID FROM route WHERE routeName LIKE ?;";
-		$query = $this->conn->prepareQuery($stmt);
-		$this->conn->bindVal($query, 1, $timetable, $this->conn->STRING);
-		$this->conn->executeQuery($query);
-		$fetchedResults = $this->conn->fetchResults($query);
-		$routeID = intval($fetchedResults[0]['routeID']);
-		$availDays = $this->checkDays($routeID);
+		$routeID = $this->fetchRouteID($timetable);
+		//$availDays = $this->checkDays($routeID);
 
 		if($availDays["monFri"] == true){
 			$stmt2 = "SELECT s.name, mt.time, mt.direction FROM midweekTime mt ";
-                	$stmt2 .= "INNER JOIN stop s ON s.stopID = mt.stopID ";
- 			$stmt2 .= "WHERE s.routeID = ? ORDER BY midweekTimeID;";
+			$stmt2 .= "INNER JOIN stop s ON mt.stopID = s.stopID ";
+			$stmt2 .= "WHERE mt.routeID = ? ORDER BY midweekTimeID;";
 			$query2 = $this->conn->prepareQuery($stmt2);
 			$this->conn->bindVal($query2, 1, $routeID, $this->conn->INT);
 			$this->conn->executeQuery($query2);
 			$fetched["monFri"] = $this->conn->fetchResults($query2);
-		}else{}
+		}
 
 		if($availDays["sat"] == true){
 			$stmt3 = "SELECT s.name, st.time, st.direction FROM satTime st ";
@@ -201,7 +236,7 @@ class DbOps {
 			$this->conn->bindVal($query3, 1, $routeID, $this->conn->INT);
 			$this->conn->executeQuery($query3);
 			$fetched["sat"] = $this->conn->fetchResults($query3);
-		}else{}
+		}
 
 		if($availDays["sun"] == true){
 			$stmt4 = "SELECT s.name, st.time, st.direction FROM sunTime st ";
@@ -211,7 +246,7 @@ class DbOps {
 			$this->conn->bindVal($query4, 1, $routeID, $this->conn->INT);
 			$this->conn->executeQuery($query4);
 			$fetched["sun"] = $this->conn->fetchResults($query4);
-		}else{}
+		}
 
 		return $fetched;
 	}
@@ -240,6 +275,16 @@ class DbOps {
 		else{$results["sun"] = true;}
 
 		return $results;
+	}
+
+	private function fetchRouteID($route){
+		$stmt = "SELECT routeID FROM route WHERE routeName LIKE ?;";
+		$query = $this->conn->prepareQuery($stmt);
+		$this->conn->bindVal($query, 1, $route, $this->conn->STRING);
+		$this->conn->executeQuery($query);
+		$fetchedResults = $this->conn->fetchResults($query);
+		$routeID = intval($fetchedResults[0]['routeID']);
+		return $routeID;
 	}
 }
 
--- public/api/v1/index.php
+++ public/api/v1/index.php
@@ -2,7 +2,7 @@
 /**
   * @author Stefan Burke
   * @author Stefan Burke <stefan.burke@mydit.ie>
-  */
+**/
 date_default_timezone_set('Europe/Dublin');
 
 require_once '../include/DbOps.php';
@@ -14,6 +14,29 @@ require '../libs/slim/Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
 $app = new \Slim\Slim();
+
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+}
+
+// instead of mapping:
+$app->options('/(:x+)', function() use ($app) {
+    //...return correct headers...
+    $app->response->setStatus(200);
+});
 
 $app->post('/login', function() use ($app){
 	$dbManager = new DBManager();
@@ -113,8 +136,9 @@ $app->post('/updateLocation', function() use ($app){
 	$time = $parseTime[0]. " " .$parseTime2[0];
 
 	$deviceID = $data['deviceID'];
+	$routeID = $data['routeID'];
 
-	$result = $db->updateLocation($lat, $lon, $time, $deviceID);
+	$result = $db->updateLocation($lat, $lon, $time, $deviceID, $routeID);
 	if($result == "success"){
 		$response["error"] = false;
 		$response["message"] = "success";
@@ -152,48 +176,74 @@ $app->post('/updateRouteMap', function() use ($app){
 	$dbManager->closeConn();
 });
 
-$app->get('/getLocation', function() use ($app){
-	$dbManager = new DBManager();
-    	$db = new DbOps($dbManager);
-    	$dbManager->openConn();
+$app->get('/getLocation/:route', function($route) use ($app){
+	$headerCheck = headerCheck();
+	if($headerCheck == "invalid" || $headerCheck == "expired"){
+		$response["error"] = true;
+		$response["message"] = "invalid/expired token";
+		sendResp(HTTPSTATUS_OK, $response);
+	}else{
+		$dbManager = new DBManager();
+    		$db = new DbOps($dbManager);
+    		$dbManager->openConn();
+		var_dump($headers);
 
-    	$response = array();
-	$result = $db->getLocation();
-	$response["error"] = false;
+    		$response = array();
+		$result = $db->getLocation($route);
+		$response["error"] = false;
 
-	$response["data"] = array('locations' => $result);
-	sendResp(HTTPSTATUS_OK, $response);
-	$dbManager->closeConn();
+		if($result == false){
+			$response["data"] = "No Data Available";
+		}else{
+			$response["data"] = $result;
+		}
+
+		sendResp(HTTPSTATUS_OK, $response);
+		$dbManager->closeConn();
+	}
 });
 
 $app->get('/getRouteMap/:route', function($route) use ($app){
-	$dbManager = new DBManager();
-	$db = new DbOps($dbManager);
-	$dbManager->openConn();
+	$headerCheck = headerCheck();
 
-	$response = array();
-	$result = $db->getRoute($route);
+	if($headerCheck == "invalid" || $headerCheck == "expired"){
+		$response = "invalid/expired token";
+		sendResp(HTTPSTATUS_OK, $response);
+	}else{
+		$dbManager = new DBManager();
+		$db = new DbOps($dbManager);
+		$dbManager->openConn();
 
-	$response["error"] = false;
-	$response["data"] = array('route' => $result);
+		$response = array();
+		$result = $db->getRoute($route);
 
-	sendResp(HTTPSTATUS_OK, $response);
-	$dbManager->closeConn();
+		$response["error"] = false;
+		$response["data"] = array('route' => $result);
+
+		sendResp(HTTPSTATUS_OK, $response);
+		$dbManager->closeConn();
+	}
 });
 
 $app->get('/getStops/:route', function($route) use ($app){
-	$dbManager = new DBManager();
-    	$db = new DbOps($dbManager);
-    	$dbManager->openConn();
+	/*$headerCheck = headerCheck();
+	if($headerCheck == "invalid" || $headerCheck == "expired"){
+		$response = "invalid/expired token";
+		sendResp(HTTPSTATUS_OK, $response);
+	}else{*/
+		$dbManager = new DBManager();
+    		$db = new DbOps($dbManager);
+    		$dbManager->openConn();
 
-	$response = array();
-	$result = $db->getStops($route);
+		$response = array();
+		$result = $db->getStops($route);
 
-	$response["error"] = false;
+		$response["error"] = false;
 
-	$response["data"] = array('stops' => $result);
-	sendResp(HTTPSTATUS_OK, $response);
-	$dbManager->closeConn();
+		$response["data"] = array('stops' => $result);
+		sendResp(HTTPSTATUS_OK, $response);
+		$dbManager->closeConn();
+	//}
 });
 
 $app->get('/getTimetable/:name', function($timetable) use ($app){
@@ -209,32 +259,35 @@ $app->get('/getTimetable/:name', function($timetable) use ($app){
 	sendResp(HTTPSTATUS_OK, $response);
 	$dbManager->closeConn();
 });
-/*function authenticate(\Slim\Route\ $route){
-	$header = apache_request_headers();
-	$response = array();
-	$app = \Slim\Slim::getInstance();
 
-	if(isset($header['Auth'])){
-		$db = new DbOps();
+function headerCheck(){
+	$headers = apache_request_headers();
 
-		$token = $header['Auth'];
-		if(!$db->tokenCheck($token)){
-			$response["error"] = true;
-			$response["message"] = "invalid token";
-			sendResp(401, $response);
-			$app->stop();
-		}else if(db->hasTokenExpired($token)){
-			$response["error"] = true;
-			$response["message"] = "expired token";
-			sendResp(401, $response);
-			$app->stop();
-		}else {
-			$tokenExpire = date('Y-m-d H:i:s', strtotime('+1 hour'));
-			$db->keepTokenAlive($token, $tokenExpire);
-			$this->next->call();
-		}
+	if(!isset($headers["Auth"]) && $headers["Auth"] == false){
+		return "invalid";
 	}
-}*/
+	else{
+		$dbManager = new DBManager();
+    		$db = new DbOps($dbManager);
+    		$dbManager->openConn();
+
+		$token = $headers['Auth'];
+		if(!$db->tokenCheck($token)){
+			return  "invalid";
+		}
+		else{
+			if($db->hasTokenExpired($token)){
+				return "expired";
+			}else {
+				$tokenExpire = date('Y-m-d H:i:s', strtotime('+1 hour'));
+				$db->keepTokenAlive($token, $tokenExpire);
+				return "updated";
+			}
+		}
+
+		$dbManager->closeConn();
+	}
+}
 
 function sendResp($respCode, $resp){
 	$app = \Slim\Slim::getInstance();
